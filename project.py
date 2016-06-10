8fc5f121ba8f5cf4e9d33d31a305fd0fb06b294e
Calvin Nguyen <cpnguyen@calvin-yoga314.(none)>
2016-06-09 23:10:46 -0700

    Functional draft of csv import with null values for blank csv values

--- project.py
+++ project.py
@@ -11,29 +11,34 @@ def get_path():
 
 def create_table(path, data):
 
-    db_file = os.path.join(path, 'cms2.db')
+    db_file = os.path.join(path, 'cms1.db')
     schema_file = os.path.join(path, 'cms_schema.sql')
+    update_file = os.path.join(path, 'update.sql')
 
     db_is_new = not os.path.exists(db_file)
     #Connecting/Creating database file
     conn = sqlite3.connect(db_file)
     c = conn.cursor()
-    #
-    # if db_is_new:
-    #     print "Database created, creating table(s) schema"
-    #     print "Reading SQL script..."
-    #     f_schema = open(schema_file, 'r')
-    #     schema = f_schema.read()
-    #     f_schema.close()
-    #     c.executescript(schema) #create schema
+
+    if db_is_new:
+        print "Database created, creating table(s) schema"
+        print "Reading SQL script..."
+        f_schema = open(schema_file, 'r')
+        schema = f_schema.read()
+        f_schema.close()
+        c.executescript(schema) #create schema
 
 
-    # else:
-    #     print "Database exists; opened successfully"
+    else:
+        print "Database exists; opened successfully"
 
     conn.text_factory= lambda x: unicode(x, 'utf-8', 'ignore')
-    cms2 = data.to_sql(db_file, con=conn, flavor='sqlite', if_exists='append')
-    print cms2
+    cms2 = data.to_sql('report', con=conn, flavor='sqlite', if_exists='append', index=False)
+
+    f_update = open(update_file, 'r')
+    update = f_update.read()
+    f_update.close()
+    c.executescript(update)
     conn.commit()
     return cms2
 
@@ -132,55 +137,58 @@ data_type = {"npi": int, "provider_last_name": str, "provider_first_name": str, 
            "average_HCC_risk_score_of_beneficiaries": float
 }
 
-str_conv = lambda x: str("unknown") if x=='' else x
-int_conv = lambda x: int(999) if x=='' else x
-float_conv = lambda x: float(999) if x=='' else x
+# str_conv = lambda x: str("unknown") if x=='' else x
+# int_conv = lambda x: int(999) if x=='' else x
+# float_conv = lambda x: float(999) if x=='' else x
 data = pd.read_csv(os.path.join(get_path(),'Medicare_Physician_and_Other_Supplier_National_Provider_Identifier__NPI__Aggregate_Report__Calendar_Year_2014.csv'),
-                                sep=',', names=columns, dtype=data_type,
-                                converters={"npi": int_conv, "provider_last_name": str_conv, "provider_first_name": str_conv, "provider_middle_initial": str_conv,
-                               "provider_credentials": str_conv, "provider_gender": str_conv, "provider_entity_type": str_conv,
-                               "provider_street_address_1": str_conv, "provider_street_address_2": str_conv, "provider_city": str_conv,
-                               "provider_zip_code": int_conv, "provider_state_code": str_conv, "provider_country_code": str_conv,
-                               "provider_type": str_conv, "medicare_participation_indicator": str_conv, "number_of_HCPCS": int_conv,
-                               "number_of_services": int_conv, "number_of_medicare_beneficiaries": int_conv,
-                               "total_submitted_charge_amount": float_conv, "total_medicare_allowed_amount": float_conv,
-                               "total_medicare_payment_amount": float_conv, "total_medicare_standardized_payment_amount": float_conv,
-                               "drug_suppress_indicator": str_conv, "number_of_HCPCS_associated_with_drug_services": int_conv,
-                               "number_of_drug_services": int_conv, "number_of_medicare_beneficiaries_with_drug_services": int_conv,
-                               "total_drug_submitted_charge_amount": float_conv, "total_drug_medicare_allowed_amount": float_conv,
-                               "total_drug_medicare_payment_amount": float_conv, "total_drug_medicare_standardized_payment_amount": float_conv,
-                               "medical_suppress_indicator": str_conv, "number_of_HCPCS_associated_medical_services": int_conv,
-                               "number_of_medical_services": int_conv, "number_of_medicare_beneficiaries_with_medical_services": int_conv,
-                               "total_medical_submitted_charge_amount": float_conv, "total_medical_medicare_allowed_amount": float_conv,
-                               "total_medical_medicare_payment_amount": float_conv, "total_medical_medicare_standardized_payment_amount": float_conv,
-                               "average_age_of_beneficiaries": int_conv, "number_of_beneficiaries_age_less_65": int_conv,
-                               "number_of_beneficiaries_age_65_to_74": int_conv, "number_of_beneficiaries_age_75_to_84": int_conv,
-                               "number_of_beneficiaries_age_greater_84": int_conv,"number_of_female_beneficiaries": int_conv,
-                               "number_of_male_beneficiaries": int_conv, "number_of_non_hispanic_white_beneficiaries": int_conv,
-                               "number_of_african_american_beneficiaries": int_conv, "number_of_asian_pacific_islander_beneficiaries": int_conv,
-                               "number_of_hispanic_beneficiaries": int_conv, "number_of_american_indian_alaskan_native_beneficiaries": int_conv,
-                               "number_of_beneficiaries_with_race_not_elsewhere_classified": int_conv,
-                               "number_of_beneficiaries_with_medicare_only_entitlement": int_conv,
-                               "number_of_beneficiaries_with_medicare_and_medicaid_entitlement": int_conv,
-                               "percent_of_beneficiaries_identified_with_atrial_fibrillation": int_conv,
-                               "percent_of_beneficiaries_identified_with_alzheimers_disease_or_dementia": int_conv,
-                               "percent_of_beneficiaries_identified_with_asthma": int_conv, "percent_of_beneficiaries_identified_with_cancer": int_conv,
-                               "percent_of_beneficiaries_identified_with_heart_failure": int_conv,
-                               "percent_of_beneficiaries_identified_with_chronic_kidney_disease": int_conv,
-                               "percent_of_beneficiaries_identified_with_chronic_obstructive_pulmonary_disease": int_conv,
-                               "percent_of_beneficiaries_identified_with_depression": int_conv,
-                               "percent_of_beneficiaries_identified_with_diabetes": int_conv,
-                               "percent_of_beneficiaries_identified_with_hyperlipidemia": int_conv,
-                               "percent_of_beneficiaries_identified_with_hypertension": int_conv,
-                               "percent_of_beneficiaries_identified_with_ischemic_heart_disease": int_conv,
-                               "percent_of_beneficiaries_identified_with_osteoporosis": int_conv,
-                               "percent_of_beneficiaries_identified_with_rheumatoid_arthritis_osteoarthritis": int_conv,
-                               "percent_of_beneficiaries_identified_with_schizophrenia_other_psychotic_disorders": int_conv,
-                               "percent_of_beneficiaries_identified_with_stroke": int_conv,
-                               "average_HCC_risk_score_of_beneficiaries": float_conv})
-# print data.isnull()
+                   sep=',', names=columns
+                   # na_values=[''],
+                   # dtype=data_type,
+                   #              converters={"npi": int_conv, "provider_last_name": str_conv, "provider_first_name": str_conv, "provider_middle_initial": str_conv,
+                   #             "provider_credentials": str_conv, "provider_gender": str_conv, "provider_entity_type": str_conv,
+                   #             "provider_street_address_1": str_conv, "provider_street_address_2": str_conv, "provider_city": str_conv,
+                   #             "provider_zip_code": int_conv, "provider_state_code": str_conv, "provider_country_code": str_conv,
+                   #             "provider_type": str_conv, "medicare_participation_indicator": str_conv, "number_of_HCPCS": int_conv,
+                   #             "number_of_services": int_conv, "number_of_medicare_beneficiaries": int_conv,
+                   #             "total_submitted_charge_amount": float_conv, "total_medicare_allowed_amount": float_conv,
+                   #             "total_medicare_payment_amount": float_conv, "total_medicare_standardized_payment_amount": float_conv,
+                   #             "drug_suppress_indicator": str_conv, "number_of_HCPCS_associated_with_drug_services": int_conv,
+                   #             "number_of_drug_services": int_conv, "number_of_medicare_beneficiaries_with_drug_services": int_conv,
+                   #             "total_drug_submitted_charge_amount": float_conv, "total_drug_medicare_allowed_amount": float_conv,
+                   #             "total_drug_medicare_payment_amount": float_conv, "total_drug_medicare_standardized_payment_amount": float_conv,
+                   #             "medical_suppress_indicator": str_conv, "number_of_HCPCS_associated_medical_services": int_conv,
+                   #             "number_of_medical_services": int_conv, "number_of_medicare_beneficiaries_with_medical_services": int_conv,
+                   #             "total_medical_submitted_charge_amount": float_conv, "total_medical_medicare_allowed_amount": float_conv,
+                   #             "total_medical_medicare_payment_amount": float_conv, "total_medical_medicare_standardized_payment_amount": float_conv,
+                   #             "average_age_of_beneficiaries": int_conv, "number_of_beneficiaries_age_less_65": int_conv,
+                   #             "number_of_beneficiaries_age_65_to_74": int_conv, "number_of_beneficiaries_age_75_to_84": int_conv,
+                   #             "number_of_beneficiaries_age_greater_84": int_conv,"number_of_female_beneficiaries": int_conv,
+                   #             "number_of_male_beneficiaries": int_conv, "number_of_non_hispanic_white_beneficiaries": int_conv,
+                   #             "number_of_african_american_beneficiaries": int_conv, "number_of_asian_pacific_islander_beneficiaries": int_conv,
+                   #             "number_of_hispanic_beneficiaries": int_conv, "number_of_american_indian_alaskan_native_beneficiaries": int_conv,
+                   #             "number_of_beneficiaries_with_race_not_elsewhere_classified": int_conv,
+                   #             "number_of_beneficiaries_with_medicare_only_entitlement": int_conv,
+                   #             "number_of_beneficiaries_with_medicare_and_medicaid_entitlement": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_atrial_fibrillation": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_alzheimers_disease_or_dementia": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_asthma": int_conv, "percent_of_beneficiaries_identified_with_cancer": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_heart_failure": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_chronic_kidney_disease": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_chronic_obstructive_pulmonary_disease": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_depression": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_diabetes": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_hyperlipidemia": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_hypertension": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_ischemic_heart_disease": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_osteoporosis": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_rheumatoid_arthritis_osteoarthritis": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_schizophrenia_other_psychotic_disorders": int_conv,
+                   #             "percent_of_beneficiaries_identified_with_stroke": int_conv,
+                   #             "average_HCC_risk_score_of_beneficiaries": float_conv}
+                   )
 
 
+# print type(data["provider_middle_initial"][1]), data["provider_middle_initial"][1]
 print create_table(get_path(), data)
 
 

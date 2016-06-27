9fcfa5adb51865fa91a1b853816c63499f0bdf0d
Calvin Nguyen <cpnguyen@calvin-yoga314.(none)>
2016-06-27 16:04:34 -0700

    Corrected dtype warning -- updated Pandas read_csv param

--- project.py
+++ project.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import csv
 
+
 def get_path():
     f_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'api', 'dataset')
     return f_name
@@ -39,63 +40,12 @@ def readCSV():
                "percent_of_beneficiaries_identified_with_schizophrenia_other_psychotic_disorders",
                "percent_of_beneficiaries_identified_with_stroke", "average_HCC_risk_score_of_beneficiaries"]
 
-    data_type = {"npi": int, "provider_last_name": str, "provider_first_name": str, "provider_middle_initial": str,
-                 "provider_credentials": str,
-                 "provider_gender": str, "provider_entity_type": str, "provider_street_address_1": str,
-                 "provider_street_address_2": str, "provider_city": str,
-                 "provider_zip_code": int, "provider_state_code": str, "provider_country_code": str, "provider_type": str,
-                 "medicare_participation_indicator": str,
-                 "number_of_HCPCS": int, "number_of_services": int, "number_of_medicare_beneficiaries": int,
-                 "total_submitted_charge_amount": float,
-                 "total_medicare_allowed_amount": float, "total_medicare_payment_amount": float,
-                 "total_medicare_standardized_payment_amount": float,
-                 "drug_suppress_indicator": str, "number_of_HCPCS_associated_with_drug_services": int,
-                 "number_of_drug_services": int,
-                 "number_of_medicare_beneficiaries_with_drug_services": int, "total_drug_submitted_charge_amount": float,
-                 "total_drug_medicare_allowed_amount": float,
-                 "total_drug_medicare_payment_amount": float, "total_drug_medicare_standardized_payment_amount": float,
-                 "medical_suppress_indicator": str,
-                 "number_of_HCPCS_associated_medical_services": int, "number_of_medical_services": int,
-                 "number_of_medicare_beneficiaries_with_medical_services": int,
-                 "total_medical_submitted_charge_amount": float, "total_medical_medicare_allowed_amount": float,
-                 "total_medical_medicare_payment_amount": float,
-                 "total_medical_medicare_standardized_payment_amount": float, "average_age_of_beneficiaries": int,
-                 "number_of_beneficiaries_age_less_65": int,
-                 "number_of_beneficiaries_age_65_to_74": int, "number_of_beneficiaries_age_75_to_84": int,
-                 "number_of_beneficiaries_age_greater_84": int,
-                 "number_of_female_beneficiaries": int, "number_of_male_beneficiaries": int,
-                 "number_of_non_hispanic_white_beneficiaries": int,
-                 "number_of_african_american_beneficiaries": int, "number_of_asian_pacific_islander_beneficiaries": int,
-                 "number_of_hispanic_beneficiaries": int,
-                 "number_of_american_indian_alaskan_native_beneficiaries": int,
-                 "number_of_beneficiaries_with_race_not_elsewhere_classified": int,
-                 "number_of_beneficiaries_with_medicare_only_entitlement": int,
-                 "number_of_beneficiaries_with_medicare_and_medicaid_entitlement": int,
-                 "percent_of_beneficiaries_identified_with_atrial_fibrillation": int,
-                 "percent_of_beneficiaries_identified_with_alzheimers_disease_or_dementia": int,
-                 "percent_of_beneficiaries_identified_with_asthma": int,
-                 "percent_of_beneficiaries_identified_with_cancer": int,
-                 "percent_of_beneficiaries_identified_with_heart_failure": int,
-                 "percent_of_beneficiaries_identified_with_chronic_kidney_disease": int,
-                 "percent_of_beneficiaries_identified_with_chronic_obstructive_pulmonary_disease": int,
-                 "percent_of_beneficiaries_identified_with_depression": int,
-                 "percent_of_beneficiaries_identified_with_diabetes": int,
-                 "percent_of_beneficiaries_identified_with_hyperlipidemia": int,
-                 "percent_of_beneficiaries_identified_with_hypertension": int,
-                 "percent_of_beneficiaries_identified_with_ischemic_heart_disease": int,
-                 "percent_of_beneficiaries_identified_with_osteoporosis": int,
-                 "percent_of_beneficiaries_identified_with_rheumatoid_arthritis_osteoarthritis": int,
-                 "percent_of_beneficiaries_identified_with_schizophrenia_other_psychotic_disorders": int,
-                 "percent_of_beneficiaries_identified_with_stroke": int,
-                 "average_HCC_risk_score_of_beneficiaries": float}
-
-
     df = pd.read_csv(os.path.join(get_path(),
                                     'Medicare_Physician_and_Other_Supplier_National_Provider_Identifier__NPI__Aggregate_Report__Calendar_Year_2014.csv'),
-                       sep=',', names=columns
+                       sep=',', names=columns, header=0
                        # na_values=[''],
                        # dtype=data_type
-                       )
+                     )
     return df
 
 def create_table():

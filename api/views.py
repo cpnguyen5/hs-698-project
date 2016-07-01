e0daaa02a9091eba347a6072b4c420934aee5416
Calvin Nguyen <cpnguyen@calvin-yoga314.(none)>
2016-07-01 16:52:22 -0700

    Updated state() and map() functions to incorporate sqlalchemy query syntax instead of sqlite3

--- api/views.py
+++ api/views.py
@@ -6,7 +6,9 @@ import sqlite3
 import numpy as np
 import pandas as pd
 import json
-
+from .models import Report
+from api import db
+from sqlalchemy import func
 
 def get_db():
     db_path=os.path.join(project.get_path(),'cms3.db')
@@ -23,93 +25,107 @@ def home():
 @app.route('/state')
 def state():
 
-    db_file = get_db()
-    conn = sqlite3.connect(db_file)  # create/open database
-    conn.row_factory=sqlite3.Row
+    rows = db.session.query(Report.provider_state_code, func.avg(Report.percent_of_beneficiaries_identified_with_cancer)).\
+        order_by(Report.provider_state_code).group_by(Report.provider_state_code).all()
+    state = rows
 
-    with conn:
-        c = conn.cursor()
-
-        c.execute('''SELECT provider_state_code, percent_of_beneficiaries_identified_with_cancer
-        FROM report
-
-        ORDER BY provider_state_code;''')
-        # GROUP BY provider_state_code
-        rows = c.fetchall()
-
-    states_dict={}
-    for row in rows:
-        # print row["provider_state_code"]
-        key = str(row[0])
-        if row[1] != None and isinstance(row[1], int):
-            if states_dict.has_key(key):
-                states_dict[key]+=[row[1]]
-            else:
-                states_dict[str(key)]=[row[1]]
-        else:
-            pass
-
-    # for elem in states_dict.items():
-    #     print elem
-    dict = sorted(states_dict.items())
-    for i, value in enumerate(dict):
-        temp=list(dict[i])
-        temp[1]=(sum(dict[i][1])) / (len(dict[i][1]))
-        dict[i]=tuple(temp)
-        # print dict[i]
-    # for i in range(len(dict)):
-    #     print dict[i][0], sum(dict[i][1])
-        # dict[i][1]=sum(dict[i][1])
-    return render_template("state.html", rows=dict)
+    # db_file = get_db()
+    # conn = sqlite3.connect(db_file)  # create/open database
+    # conn.row_factory=sqlite3.Row
+    #
+    # with conn:
+    #     c = conn.cursor()
+    #
+    #     c.execute('''SELECT provider_state_code, percent_of_beneficiaries_identified_with_cancer
+    #     FROM report
+    #
+    #     ORDER BY provider_state_code;''')
+    #     # GROUP BY provider_state_code
+    #     rows = c.fetchall()
+    #
+    # states_dict={}
+    # for row in rows:
+    #     # print row["provider_state_code"]
+    #     key = str(row[0])
+    #     if row[1] != None and isinstance(row[1], int):
+    #         if states_dict.has_key(key):
+    #             states_dict[key]+=[row[1]]
+    #         else:
+    #             states_dict[str(key)]=[row[1]]
+    #     else:
+    #         pass
+    #
+    # # for elem in states_dict.items():
+    # #     print elem
+    # dict = sorted(states_dict.items())
+    # for i, value in enumerate(dict):
+    #     temp=list(dict[i])
+    #     temp[1]=(sum(dict[i][1])) / (len(dict[i][1]))
+    #     dict[i]=tuple(temp)
+    #     # print dict[i]
+    # # for i in range(len(dict)):
+    # #     print dict[i][0], sum(dict[i][1])
+    #     # dict[i][1]=sum(dict[i][1])
+    return render_template("state.html", rows=state)
 
 
 @app.route('/map')
 def map():
 
-    db_file = get_db()
-    conn = sqlite3.connect(db_file)  # create/open database
-    conn.row_factory = sqlite3.Row
+    rows = db.session.query(Report.provider_state_code, func.avg(Report.percent_of_beneficiaries_identified_with_cancer)).\
+        filter(Report.provider_state_code != 'DC').order_by(Report.provider_state_code).group_by(Report.provider_state_code).all()
 
-    with conn:
-        c = conn.cursor()
+    state_lst=[]
+    for i in range(len(rows)):
+        state = tuple()
+        state += (rows[i][0],)
+        state += (round(rows[i][1],2),)
+        state_lst+=[state]
+    dict_state = dict(state_lst)
 
-        c.execute('''SELECT provider_state_code, percent_of_beneficiaries_identified_with_cancer
-         FROM report
+    # db_file = get_db()
+    # conn = sqlite3.connect(db_file)  # create/open database
+    # conn.row_factory = sqlite3.Row
+    # #
+    # with conn:
+    #     c = conn.cursor()
+    #     #
+    #     c.execute('''SELECT provider_state_code, percent_of_beneficiaries_identified_with_cancer
+    #      FROM report
+    #
+    #      ORDER BY provider_state_code;''')
+    #     # GROUP BY provider_state_code
+    #     rows = c.fetchall()
 
-         ORDER BY provider_state_code;''')
-        # GROUP BY provider_state_code
-        rows = c.fetchall()
+    # #Create dictionary with state-value pairs -- values are list of values
+    # states_dict = {}
+    # for row in rows:
+    #     # print row["provider_state_code"]
+    #     key = str(row[0])
+    #     if row[1] != None and isinstance(row[1], int):
+    #         if states_dict.has_key(key):
+    #             states_dict[key] += [row[1]]
+    #         else:
+    #             states_dict[str(key)] = [row[1]]
+    #     else:
+    #         pass
 
-    #Create dictionary with state-value pairs -- values are list of values
-    states_dict = {}
-    for row in rows:
-        # print row["provider_state_code"]
-        key = str(row[0])
-        if row[1] != None and isinstance(row[1], int):
-            if states_dict.has_key(key):
-                states_dict[key] += [row[1]]
-            else:
-                states_dict[str(key)] = [row[1]]
-        else:
-            pass
-
-    #Obtain list of tuples with key-value pairs of state-average prevalence
-    lst_pair = sorted(states_dict.items())
-    for i, value in enumerate(lst_pair):
-        temp = list(lst_pair[i])
-        temp[1] = (sum(lst_pair[i][1])) / (len(lst_pair[i][1]))
-        lst_pair[i] = tuple(temp)
-
-    state = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA',
-             'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK',
-             'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
-    #filter list for actual states
-    lst_state = []
-    for elem in lst_pair:
-        if str(elem[0]) in state:
-            lst_state += [elem]
-
-    dict_state=dict(lst_state)
+    # #Obtain list of tuples with key-value pairs of state-average prevalence
+    # lst_pair = sorted(states_dict.items())
+    # for i, value in enumerate(lst_pair):
+    #     temp = list(lst_pair[i])
+    #     temp[1] = (sum(lst_pair[i][1])) / (len(lst_pair[i][1]))
+    #     lst_pair[i] = tuple(temp)
+    #
+    # state = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA',
+    #          'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK',
+    #          'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+    # #filter list for actual states
+    # lst_state = []
+    # for elem in lst_pair:
+    #     if str(elem[0]) in state:
+    #         lst_state += [elem]
+    # dict_state=dict(lst_state)
     return render_template("map.html", d_state=dict_state, js_file=url_for('static',
                                                                            filename='js/datamaps.usa.min.js'))
 
@@ -183,3 +199,4 @@ def data():
 
 
 ##main
+# state()

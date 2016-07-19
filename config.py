851a2ee5e795a1f9059c2eba020236a7ded0d1ab
Calvin Nguyen <cpnguyen@calvin-yoga314.(none)>
2016-07-18 21:35:43 -0700

    Corrected readPUF() and initdb() functions to insert data by chunks

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 basedir=os.path.abspath(os.path.dirname(__file__))
 db_path=os.path.join(basedir, 'api', 'dataset')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(db_path, 'cms3.db') #required by Flask-SQLAlchemy extension -- path to db file
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(db_path, 'cms.db') #required by Flask-SQLAlchemy extension -- path to db file
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- project.py
+++ project.py
@@ -10,6 +10,7 @@ from api import db
 from api.models import Report, Puf, Cancer
 import urllib
 import csv
+from config import SQLALCHEMY_DATABASE_URI
 
 
 def get_path():
@@ -46,57 +47,6 @@ def readCSV():
                "percent_of_beneficiaries_identified_with_osteoporosis", "percent_of_beneficiaries_identified_with_rheumatoid_arthritis_osteoarthritis",
                "percent_of_beneficiaries_identified_with_schizophrenia_other_psychotic_disorders",
                "percent_of_beneficiaries_identified_with_stroke", "average_HCC_risk_score_of_beneficiaries"]
-    # types = {'npi': np.int64, 'provider_last_name': 'S20', 'provider_first_name': 'S20',
-    #          'provider_middle_initial': 'S20', 'provider_credentials': 'S20',
-    #          'provider_gender': 'S20', 'provider_entity_type': 'S20', 'provider_street_address_1': 'S20',
-    #          'provider_street_address_2': 'S20',
-    #          'provider_city': 'S20', 'provider_zip_code': 'S20', 'provider_state_code': 'S20',
-    #          'provider_country_code': 'S20', 'provider_type': 'S20',
-    #          'medicare_participation_indicator': 'S20', 'number_of_HCPCS': np.int64, 'number_of_services': np.float64,
-    #          'number_of_medicare_beneficiaries': np.int64,
-    #          'total_submitted_charge_amount': np.float64, 'total_medicare_allowed_amount': np.float64,
-    #          'total_medicare_payment_amount': np.float64,
-    #          'total_medicare_standardized_payment_amount': np.float64, 'drug_suppress_indicator': 'S20',
-    #          'number_of_HCPCS_associated_with_drug_services': np.float64,
-    #          'number_of_drug_services': np.float64, 'number_of_medicare_beneficiaries_with_drug_services': np.float64,
-    #          'total_drug_submitted_charge_amount': np.float64,
-    #          'total_drug_medicare_allowed_amount': np.float64, 'total_drug_medicare_payment_amount': np.float64,
-    #          'total_drug_medicare_standardized_payment_amount': np.float64,
-    #          'medical_suppress_indicator': 'S20', 'number_of_HCPCS_associated_medical_services': np.float64,
-    #          'number_of_medical_services': np.float64,
-    #          'number_of_medicare_beneficiaries_with_medical_services': np.float64,
-    #          'total_medical_submitted_charge_amount': np.float64,
-    #          'total_medical_medicare_allowed_amount': np.float64, 'total_medical_medicare_payment_amount': np.float64,
-    #          'total_medical_medicare_standardized_payment_amount': np.float64,
-    #          'average_age_of_beneficiaries': np.int64, 'number_of_beneficiaries_age_less_65': np.float64,
-    #          'number_of_beneficiaries_age_65_to_74': np.float64,
-    #          'number_of_beneficiaries_age_75_to_84': np.float64, 'number_of_beneficiaries_age_greater_84': np.float64,
-    #          'number_of_female_beneficiaries': np.float64,
-    #          'number_of_male_beneficiaries': np.float64, 'number_of_non_hispanic_white_beneficiaries': np.float64,
-    #          'number_of_african_american_beneficiaries': np.float64,
-    #          'number_of_asian_pacific_islander_beneficiaries': np.float64,
-    #          'number_of_hispanic_beneficiaries': np.float64,
-    #          'number_of_american_indian_alaskan_native_beneficiaries': np.float64,
-    #          'number_of_beneficiaries_with_race_not_elsewhere_classified': np.float64,
-    #          'number_of_beneficiaries_with_medicare_only_entitlement': np.float64,
-    #          'number_of_beneficiaries_with_medicare_and_medicaid_entitlement': np.float64,
-    #          'percent_of_beneficiaries_identified_with_atrial_fibrillation': np.float64,
-    #          'percent_of_beneficiaries_identified_with_alzheimers_disease_or_dementia': np.float64,
-    #          'percent_of_beneficiaries_identified_with_asthma': np.float64,
-    #          'percent_of_beneficiaries_identified_with_cancer': np.float64,
-    #          'percent_of_beneficiaries_identified_with_heart_failure': np.float64,
-    #          'percent_of_beneficiaries_identified_with_chronic_kidney_disease': np.float64,
-    #          'percent_of_beneficiaries_identified_with_chronic_obstructive_pulmonary_disease': np.float64,
-    #          'percent_of_beneficiaries_identified_with_depression': np.float64,
-    #          'percent_of_beneficiaries_identified_with_diabetes': np.float64,
-    #          'p-ercent_of_beneficiaries_identified_with_hyperlipidemia': np.float64,
-    #          'percent_of_beneficiaries_identified_with_hypertension': np.float64,
-    #          'percent_of_beneficiaries_identified_with_ischemic_heart_disease': np.float64,
-    #          'percent_of_beneficiaries_identified_with_osteoporosis': np.float64,
-    #          'percent_of_beneficiaries_identified_with_rheumatoid_arthritis_osteoarthritis': np.float64,
-    #          'percent_of_beneficiaries_identified_with_schizophrenia_other_psychotic_disorders': np.float64,
-    #          'percent_of_beneficiaries_identified_with_stroke': np.float64,
-    #          'average_HCC_risk_score_of_beneficiaries': np.float64}
     types = [('npi', np.int64), ('provider_last_name', 'S20'), ('provider_first_name', 'S20'),
              ('provider_middle_initial', 'S20'), ('provider_credentials', 'S20'),
              ('provider_gender', 'S20'), ('provider_entity_type', 'S20'), ('provider_street_address_1', 'S20'),
@@ -181,7 +131,7 @@ def readCSV():
 
 
 def readPUF():
-    columns = ['npi', 'provider_last_name', 'provider_first_name', 'provider_middle_initial','provider_credentials',
+    puf_columns = ['npi', 'provider_last_name', 'provider_first_name', 'provider_middle_initial','provider_credentials',
                'provider_gender', 'provider_entity_type', 'provider_street_address_1', 'provider_street_address_2',
                'provider_city', 'provider_zip_code', 'provider_state_code', 'provider_country_code', 'provider_type',
                'medicare_participation_indicator', 'place_of_service', 'HCPCS_code', 'HCPCS_description',
@@ -190,7 +140,7 @@ def readPUF():
                'average_medicare_allowed_amount', 'average_submitted_charge_amount', 'average_medicare_payment_amount',
                'average_medicare_standardized_amount']
 
-    types = [('npi', np.uint64), ('provider_last_name', 'S20'), ('provider_first_name', 'S20'), ('provider_middle_initial', 'S20'),
+    puf_types = [('npi', np.uint64), ('provider_last_name', 'S20'), ('provider_first_name', 'S20'), ('provider_middle_initial', 'S20'),
              ('provider_credentials', 'S20'),('provider_gender', 'S20'),('provider_entity_type', 'S20'),
              ('provider_street_address_1', 'S20'),('provider_street_address_2', 'S20'),('provider_city', 'S20'),
              ('provider_zip_code', 'S20'),('provider_state_code', 'S20'),('provider_country_code', 'S20'),
@@ -200,33 +150,26 @@ def readPUF():
              ('number_of_distinct_medicare_beneficiary_per_day_services', np.float64),
              ('average_medicare_allowed_amount', np.float64),('average_submitted_charge_amount', np.float64),
              ('average_medicare_payment_amount', np.float64),('average_medicare_standardized_amount', np.float64)]
-    df = pd.read_csv(os.path.join(get_path(),
-                                  'Medicare_Provider_Utilization_and_Payment_Data__Physician_and_Other_Supplier_PUF_CY2014.csv'),
-                     sep=',', names=columns, header=0, na_values='')
-                     # , dtype=types)
-    # filter for only US states -- Convert to Numpy array
-    state = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA',
-             'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK',
-             'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'DC']
 
-    terr = ['PR', 'GU', 'VI', 'AS', 'District of Columbia', 'MP', 'AA', 'AE', 'AP']  # USA territories
-    usa = state + terr
-    data = df.as_matrix()
-    US_data = np.array([row for row in data if row[12] == 'US'])
-    for row in US_data:
-        if row[11] not in usa and len(str(row[10])) >= 5:
-            location = pz.get(int(str(row[10])[:5]), 'US')
-            if location != False:
-                row[10] = location['postal_code']  # correct ZIP code
-                row[11] = location['state_short']  # correct state code
-    state_data = np.array([row for row in US_data if row[11] in state])
+    sel = ['npi', 'place_of_service', 'HCPCS_code', 'HCPCS_description',
+           'identifies_HCPCS_as_drug_included_in_the_ASP_drug_list', 'number_of_services',
+           'number_of_medicare_beneficiaries', 'number_of_distinct_medicare_beneficiary_per_day_services',
+           'average_medicare_allowed_amount', 'average_submitted_charge_amount', 'average_medicare_payment_amount',
+           'average_medicare_standardized_amount']
+    # puf_df = pd.read_csv(os.path.join(get_path(),
+    #                               'Medicare_Provider_Utilization_and_Payment_Data__Physician_and_Other_Supplier_PUF_CY2014.csv'),
+    #                  sep=',', names=puf_columns, dtype=puf_types, usecols=sel, header=0, na_values='')
+    puf_df = pd.DataFrame()
+    csv_path= os.path.join(get_path(),
+                           # 'puf_2014.csv')
+                           'Medicare_Provider_Utilization_and_Payment_Data__Physician_and_Other_Supplier_PUF_CY2014.csv')
+    reader = pd.read_csv(csv_path, iterator=True, chunksize=2000000, na_values='', names=puf_columns, dtype=puf_types, usecols=sel, header=0)
 
-    # Convert to recarray -- transfer hetergeneous column dtypes to DataFrame
-    state_recarray = np.core.records.fromarrays(np.transpose(state_data), dtype=types, names=columns)
-    # Convert to Pandas DataFrame
-    state_df = pd.DataFrame.from_records(state_recarray, columns=columns)
-    # state_df = state_df.replace(to_replace='', value=np.nan)
-    return state_df
+    pd_lst=[]
+    for chunk in reader:
+        pd_lst+=[chunk]
+        # puf_df = pd.concat([puf_df, chunk])
+    return pd_lst
 
 
 def readBCH():
@@ -315,7 +258,8 @@ def query(path):
 def init_db():
 
     # #Create engine to store data in local directory's db file
-    db_path=os.path.join(get_path(), 'cms3.db')
+    db_name = os.path.basename(SQLALCHEMY_DATABASE_URI)
+    db_path=os.path.join(get_path(), db_name) #hardcode
     engine = db.engine # sqlalchemy lib -- engine=create_engine('sqlite:///%s' % (db_path))
 
     db_is_new = not os.path.exists(db_path)
@@ -337,48 +281,26 @@ def init_db():
         df_report = readCSV()
         report_lst = df_report.to_dict(orient='records')  # orient by records to align format
         db.session.execute(Report.__table__.insert(), report_lst)
-        # df_puf = readPUF()
-        # puf_lst = df_puf.to_dict(orient='records')
-        # db.session.execute(Puf.__table__.insert(), puf_lst)
+        db.session.commit()
+        df_puf = readPUF()
+        for elem in df_puf[:5]:
+            puf_lst = elem.to_dict(orient='records')
+            db.session.execute(Puf.__table__.insert(), puf_lst)
+            db.session.commit()
         df_can = readBCH()
         can_lst = df_can.to_dict(orient='records')
         db.session.execute(Cancer.__table__.insert(), can_lst)
 
-        #Import puf data
-        # schema_file = os.path.join(get_path(), 'puf_schema.sql')
-        # f_schema = open(schema_file, 'r')
-        # schema = f_schema.read()
-        # f_schema.close()
-        # c.executescript(schema)
-        # conn.commit()
         db.session.commit() #Commit
         db.session.close() #close session
-        insertPUF()
-
-
+        print "Data insert successful...database initialization complete"
+        return
 
     else:
         print "Database exists; opened successfully"
         db.session.commit()  # Commit
         db.session.close()  # close session
-        insertPUF()
-    return
-
-
-
-def insertPUF():
-    # db_path=os.path.join(get_path(), 'cms3.db')
-    # conn = sqlite3.connect(db_path)
-    # c = conn.cursor()
-    # schema_file = os.path.join(get_path(), 'puf_schema.sql')
-    # f_schema = open(schema_file, 'r')
-    # schema = f_schema.read()
-    # f_schema.close()
-    # c.executescript(schema)
-    with open(os.path.join(get_path(), 'puf_2014.csv'), 'rb') as fin:
-        dr = csv.DictReader(fin)
-        to_db = [(i['col1'], i['col2']) for i in dr]
-        # conn.commit()
+        return
 
 
 def download():
@@ -389,17 +311,4 @@ def download():
 
 
 #main
-# data = readCSV().info()
-# print readPUF()
-# print data.to_dict(orient='records')[0]
-# print data["provider_middle_initial"][1], data["provider_middle_initial"][1]
-# print get_path()
-# print create_table()
-# print query(get_path())
-
-# init_db()
-
-# print data.isnull().sum()
-# print data.dropna()
-# print data.shape
-# print data.dropna().dtypes
+init_db()
